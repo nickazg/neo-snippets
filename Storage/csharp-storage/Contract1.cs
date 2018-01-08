538fcf425dddb4313aea25cda90816ffd8ebec4e
Nikolaj Kuntner <Amoogle@hotmail.com>
2018-01-09 00:05:58 +0100

    Put and Get functionality showcase

--- Storage/csharp-storage/Contract1.cs
+++ Storage/csharp-storage/Contract1.cs
@@ -4,12 +4,46 @@ using Neo.SmartContract.Framework.Services.System;
 using System;
 using System.Numerics;
 
-namespace csharp_storage
+namespace CSharpStorage
 {
     public class TestContract : Neo.SmartContract.Framework.SmartContract
     {
-        public static void Main()
+        public static int Main(string operation, string key, int x)
         {
+            if (operation == "print")
+            {
+                Runtime.Log("Hello nickazg");
+
+                return 1;
+            }
+
+            if (operation == "write")
+            {
+                Storage.Put(Storage.CurrentContext, key, x);
+
+                Runtime.Notify("The number was stored", key, x);
+
+                return 2;
+            }
+
+            if (operation == "read")
+            {
+                byte[] bytes = Storage.Get(Storage.CurrentContext, key);
+
+                BigInteger y = bytes.AsBigInteger();
+
+                Runtime.Notify("y", y);
+
+                for (int i = 0; i < 5; i++)
+                {
+                    y = 2 * y;
+                    Runtime.Notify("y = 2 * y", y);
+                }
+
+                return 3;
+            }
+
+            return 404;
         }
     }
 }
