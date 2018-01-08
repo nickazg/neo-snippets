0782802206769027fbfaa5fb3b5590c43547e84e
Nick <nick@Nicks-MacBook-Pro.local>
2018-01-09 09:28:15 +1300

    Basic Template Snippet

---
+++ Basics/csharp-basics/BasicTemplate.cs
@@ -0,0 +1,39 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using Neo.SmartContract.Framework.Services.System;
+using System;
+
+namespace csharp_basics
+{
+    public class ContractTemplate : Neo.SmartContract.Framework.SmartContract
+    {
+        public static Object Main(string operation, params object[] args)
+        {
+            // Transaction Type, transations will only go through if returned true.
+            if (Runtime.Trigger == TriggerType.Verification)
+            {
+                return true;
+            }
+
+            // Invocation transaction
+            else if (Runtime.Trigger == TriggerType.Application)
+            {
+                if (operation == "operation_name")
+                {
+                    // Passing the input args appropriately into our method
+                    MyMethod((byte[])args[0], (string)args[1]);
+                }
+
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public static void MyMethod(byte[] arg1, string arg2)
+        {
+            // DO STUFF
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,7 +4,9 @@
     width="300px;">
 </p>
 
-# neo-snippets
+<h1 align="center">NEO Snippets</h1>
+
+
 A NEO repository containing community created examples and information to guide NEO Development
 
 Currently Neo development information is all over the place and sometimes hard to find and keep track of. The goal of this repo is to provide a central, up-to-date place to understand how each aspect works and how experienced members of the community are using it to set a standard.
